fa412d002f45e69aeed9db06f3f659f2590aba0c
DSarateanu <sarateanud2@gmail.com>
2018-01-03 10:48:21 +0200

    Update 2017

---
+++ GisLoad_v1_11_12/src/md/cadastre/dbcontrollers/Filename.java
@@ -0,0 +1,54 @@
+package md.cadastre.dbcontrollers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import md.cadastre.dbconnection.Database;
+import md.cadastre.settingsparam.DbUtilities;
+
+public class Filename {
+	static final Logger logger = LoggerFactory.getLogger(Filename.class);
+	
+	private static PreparedStatement stmt = null;
+	private static ResultSet rs = null;
+	private static Connection connection = null;
+	
+	public static String getName(int id) {
+		String result = "-";
+        try {
+        	connection = Database.getConnectionPG();
+        	stmt = connection.prepareStatement("select * from gisadm.t_filenames_upload "
+                    + " where id = ?");
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                result = rs.getString("name");
+            }
+        } catch (SQLException ex) {
+            logger.error(ex.getMessage() + ";" + Arrays.toString(ex.getStackTrace()));
+            logger.error(DbUtilities.printSQLException(ex));
+        } finally {
+        	try {
+				Database.closeConnections(stmt, rs, connection);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	public static ArrayList<String> getFileList(String type) {
+		
+		
+		return null;
+	}
+}
