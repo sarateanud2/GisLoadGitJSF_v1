677e8b0af5d559ef92597b3d330464ce6a5c1255
DSarateanu <sarateanud2@gmail.com>
2018-01-12 09:08:23 +0200

    Update 11.01.2018

---
+++ GisLoad_v1_11_12/src/md/cadastre/properties/GetAllProperties.java
@@ -0,0 +1,43 @@
+package md.cadastre.properties;
+
+import java.util.Properties;
+
+public class GetAllProperties {
+	private Properties prop = new Properties();
+	
+	private String oracleCladiriTabel;
+	private String oracleTerenuriTabel;
+	private String gisPath;
+	private String tempPath;
+	private String importOrclConn;
+	private String importToOracle;
+	
+	public GetAllProperties() {
+		this.oracleCladiriTabel = prop.getProperty("cladiri_table_oracle");
+		this.oracleTerenuriTabel = prop.getProperty("terenuri_table_oracle");
+		this.gisPath = prop.getProperty("gispath");
+		this.tempPath = prop.getProperty("temp_path");
+		this.importOrclConn = prop.getProperty("import_orcl_conn");
+		this.importToOracle = prop.getProperty("import_to_oracle");
+	}
+	
+	public String getOracleCladiriTabel() {
+		return oracleCladiriTabel;
+	}
+	public String getOracleTerenuriTabel() {
+		return oracleTerenuriTabel;
+	}
+	public String getGisPath() {
+		return gisPath;
+	}
+	public String getTempPath() {
+		return tempPath;
+	}
+	public String getImportOrclConn() {
+		return importOrclConn;
+	}
+	public String getImportToOracle() {
+		return importToOracle;
+	}
+	
+}
